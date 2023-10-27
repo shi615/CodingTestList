a116f89d72b29d071557c32d8a8966f005f40908
shi615 <59819988+shi615@users.noreply.github.com>
2023-10-27 17:59:48 +0900

    柔軟なFizzBuzzの整理

---
+++ 柔軟なFizzBuzz/Contens.swift
@@ -0,0 +1,12 @@
+if let input = readLine() {
+  let inputStringList = input.split(separator: " ")
+  let aSList: [Int: String] = []
+  let result = ""
+
+  for i in 0..<inputStringList.count - 1 {
+    let as = inputStringList[i].split(separator: " ")
+
+  }
+
+  print(result)
+}
